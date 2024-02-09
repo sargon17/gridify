5feb4d89034deda02945b2987d5ddf82829f7949
Mykhaylo Tymofyeyev <mykhaylo.tymofyeyev@deseip.com>
2024-02-09 09:23:06 +0100

    first commit

---
+++ content.js
@@ -0,0 +1,73 @@
+let gridOverlay;
+
+const gridStyleLink = document.createElement("link");
+gridStyleLink.rel = "stylesheet";
+gridStyleLink.type = "text/css";
+gridStyleLink.href = chrome.runtime.getURL("gridOverlay.css");
+document.head.appendChild(gridStyleLink);
+
+let columns = 12;
+let gutter = 20;
+let maxWidth = 1200;
+let margin = 20;
+
+// Listen for messages from popup.js
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  // console.log("message received", request);
+
+  columns = request.columns;
+  gutter = request.gutter;
+  maxWidth = request.maxWidth;
+  margin = request.margin || 20;
+
+  if (request.message === "showGrid") {
+    showGrid();
+  } else if (request.message === "hideGrid") {
+    hideGrid();
+  }
+
+  sendResponse({ message: "message received" });
+});
+
+function showGrid() {
+  setGutter();
+  setMaxWidth();
+
+  if (gridOverlay) {
+    gridOverlay.style.display = "block";
+    return;
+  }
+
+  createGrid();
+}
+
+function hideGrid() {
+  if (gridOverlay) {
+    gridOverlay.style.display = "none";
+  }
+}
+
+const createGrid = () => {
+  gridOverlay = document.createElement("div");
+  gridOverlay.className = "gridify__grid-overlay";
+
+  const columnContainer = document.createElement("div");
+  columnContainer.className = "gridify__column-container";
+
+  for (let i = 0; i < columns; i++) {
+    const column = document.createElement("div");
+    column.className = "gridify__column";
+    columnContainer.appendChild(column);
+  }
+
+  gridOverlay.appendChild(columnContainer);
+  document.body.appendChild(gridOverlay);
+};
+
+const setGutter = () => {
+  document.querySelector(":root").style.setProperty("--gridify-gutter", `${gutter}px`);
+};
+
+const setMaxWidth = () => {
+  document.querySelector(":root").style.setProperty("--gridify-max-width", `${maxWidth}px`);
+};
---
+++ popup.js
@@ -0,0 +1,126 @@
+const updateGrid = document.getElementById("updateGrid");
+const columnsInput = document.getElementById("gridify-columns");
+const gutterInput = document.getElementById("gridify-gutter");
+const maxWidthInput = document.getElementById("gridify-max-width");
+const marginInput = document.getElementById("gridify-margin");
+const hideGridButton = document.getElementById("hideGrid");
+
+let columns = columnsInput.value;
+let gutter = gutterInput.value;
+let maxWidth = maxWidthInput.value;
+let margin = marginInput.value;
+
+window.onload = async () => {
+  await loadSettings();
+
+  showGrid();
+};
+
+updateGrid.addEventListener("click", () => {
+  columns = columnsInput.value;
+  gutter = gutterInput.value;
+  maxWidth = maxWidthInput.value;
+  showGrid();
+
+  //   window.close();
+});
+
+columnsInput.addEventListener("input", () => {
+  columns = columnsInput.value;
+  showGrid();
+});
+
+gutterInput.addEventListener("input", () => {
+  gutter = gutterInput.value;
+  showGrid();
+});
+
+maxWidthInput.addEventListener("input", () => {
+  maxWidth = maxWidthInput.value;
+  showGrid();
+});
+
+marginInput.addEventListener("input", () => {
+  margin = marginInput.value;
+  showGrid();
+});
+
+hideGridButton.addEventListener("click", () => {
+  hideGrid();
+});
+
+const showGrid = () => {
+  updateSettings();
+
+  const props = {
+    columns,
+    gutter,
+    maxWidth,
+    margin,
+  };
+
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, { message: "showGrid", ...props }, function (response) {
+      console.log(response);
+    });
+  });
+};
+
+const hideGrid = () => {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, { message: "hideGrid" }, function (response) {
+      console.log(response);
+    });
+  });
+};
+
+const updateSettings = async () => {
+  // website specific settings
+  const url = await getUrl();
+  const hostname = url.hostname;
+
+  // get the settings for the current website
+  let settings = localStorage.getItem(hostname);
+  settings = settings ? JSON.parse(settings) : {};
+
+  // update the settings
+  settings = {
+    ...settings,
+    columns,
+    gutter,
+    maxWidth,
+  };
+
+  // save the settings back to local storage
+  localStorage.setItem(hostname, JSON.stringify(settings));
+};
+
+const loadSettings = async () => {
+  const url = await getUrl();
+  const hostname = url.hostname;
+
+  // get the settings for the current website
+  let settings = localStorage.getItem(hostname);
+  settings = settings ? JSON.parse(settings) : {};
+
+  // update the inputs
+  columnsInput.value = settings.columns || 12;
+  gutterInput.value = settings.gutter || 20;
+  maxWidthInput.value = settings.maxWidth || 1200;
+
+  // update the global variables
+  columns = settings.columns || 12;
+  gutter = settings.gutter || 20;
+  maxWidth = settings.maxWidth || 1200;
+};
+
+const getUrl = async () => {
+  let url;
+
+  url = await new Promise((resolve, reject) => {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      resolve(new URL(tabs[0].url));
+    });
+  });
+  return url;
+};
